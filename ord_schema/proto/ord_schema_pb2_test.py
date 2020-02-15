47d78eba5d39fdb1d1695b049094500c503857e7
Steven Kearnes <kearnes@google.com>
2020-02-14 16:32:07 -0800

    Use 4 spaces, matching open source Google python style

--- ord_schema/proto/ord_schema_pb2_test.py
+++ ord_schema/proto/ord_schema_pb2_test.py
@@ -7,19 +7,19 @@ from ord_schema.proto import ord_schema_pb2
 
 class OrdSchemaPb2Test(absltest.TestCase):
 
-  def test_simple(self):
-    reaction = ord_schema_pb2.Reaction()
-    identifier = reaction.identifiers.add()
-    identifier.type = (
-      identifier.IdentifierType.REACTION_IDENTIFIER_TYPE_REACTION_SMILES)
-    identifier.value = 'C(C)Cl.Br>>C(C)Br.Cl'
-    self.assertTrue(reaction.IsInitialized())
-    self.assertLen(reaction.identifiers, 1)
-    self.assertFalse(reaction.HasField('setup'))
-    with self.assertRaisesRegex(ValueError,
-                                'Reaction has no field not_a_field'):
-      reaction.HasField('not_a_field')
+    def test_simple(self):
+        reaction = ord_schema_pb2.Reaction()
+        identifier = reaction.identifiers.add()
+        identifier.type = (
+            identifier.IdentifierType.REACTION_IDENTIFIER_TYPE_REACTION_SMILES)
+        identifier.value = 'C(C)Cl.Br>>C(C)Br.Cl'
+        self.assertTrue(reaction.IsInitialized())
+        self.assertLen(reaction.identifiers, 1)
+        self.assertFalse(reaction.HasField('setup'))
+        with self.assertRaisesRegex(ValueError,
+                                    'Reaction has no field not_a_field'):
+            reaction.HasField('not_a_field')
 
 
 if __name__ == '__main__':
-  absltest.main()
+    absltest.main()
--- setup.py
+++ setup.py
@@ -8,27 +8,27 @@ import subprocess
 
 
 class BuildPyCommand(build_py.build_py):
-  """Command that generates Python interface for protocol buffers."""
+    """Command that generates Python interface for protocol buffers."""
 
-  def run(self):
-    """Runs the protocol buffer compiler."""
-    protoc = spawn.find_executable('protoc')
-    for source in glob.glob('proto/*.proto'):
-      protoc_command = [protoc, '--python_out=ord_schema', source]
-      self.announce(f'running {protoc_command}')
-      subprocess.check_call(protoc_command)
-    # build_py.build_py is an old-style class, so super() doesn't work.
-    build_py.build_py.run(self)
+    def run(self):
+        """Runs the protocol buffer compiler."""
+        protoc = spawn.find_executable('protoc')
+        for source in glob.glob('proto/*.proto'):
+            protoc_command = [protoc, '--python_out=ord_schema', source]
+            self.announce(f'running {protoc_command}')
+            subprocess.check_call(protoc_command)
+        # build_py.build_py is an old-style class, so super() doesn't work.
+        build_py.build_py.run(self)
 
 
 if __name__ == '__main__':
-  setuptools.setup(
-    name='ord-schema',
-    description='Schema for the Open Reaction Database',
-    url='https://github.com/Open-Reaction-Database/ord-schema',
-    license='Apache License, Version 2.0',
-    install_requires=['absl-py>=0.9.0', 'protobuf>=3.11.0'],
-    version="0.1",
-    packages=setuptools.find_packages(),
-    cmdclass={'build_py': BuildPyCommand},
-  )
+    setuptools.setup(
+        name='ord-schema',
+        description='Schema for the Open Reaction Database',
+        url='https://github.com/Open-Reaction-Database/ord-schema',
+        license='Apache License, Version 2.0',
+        install_requires=['absl-py>=0.9.0', 'protobuf>=3.11.0'],
+        version="0.1",
+        packages=setuptools.find_packages(),
+        cmdclass={'build_py': BuildPyCommand},
+    )
