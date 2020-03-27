25335b308366726fa8cfa3c1408fb5e7d583b478
Connor Coley <connor.coley@gmail.com>
2020-03-27 17:58:23 -0400

    Cleaner import + example of recurse=False

--- ord_schema/validations_test.py
+++ ord_schema/validations_test.py
@@ -11,7 +11,6 @@ class ValidationsTest(parameterized.TestCase, absltest.TestCase):
 
     def setUp(self):
         super().setUp()
-        self.validations = validations
 
     @parameterized.named_parameters(
         ('volume', schema.Volume(value=15.0, units=schema.Volume.MILLILITER)),
@@ -19,7 +18,7 @@ class ValidationsTest(parameterized.TestCase, absltest.TestCase):
         ('mass', schema.Mass(value=32.1, units=schema.Mass.GRAM)),
     )
     def test_units(self, message):
-        self.assertEqual(self.validations.ValidateMessage(message), message)
+        self.assertEqual(validations.ValidateMessage(message), message)
 
     @parameterized.named_parameters(
         ('neg volume', 
@@ -38,49 +37,51 @@ class ValidationsTest(parameterized.TestCase, absltest.TestCase):
     )
     def test_units_should_fail(self, message, expected_error):
         with self.assertRaisesRegex((ValueError), expected_error):
-            self.validations.ValidateMessage(message)
+            validations.ValidateMessage(message)
 
     def test_orcid(self):
         message = schema.Person(orcid='0000-0001-2345-678X')
-        self.assertEqual(self.validations.ValidateMessage(message), message)
+        self.assertEqual(validations.ValidateMessage(message), message)
 
     def test_orcid_should_fail(self):
         message = schema.Person(orcid='abcd-0001-2345-678X')
         with self.assertRaisesRegex((ValueError), 'Invalid'):
-            self.validations.ValidateMessage(message)
+            validations.ValidateMessage(message)
 
     def test_reaction(self):
         message = schema.Reaction()
         with self.assertRaisesRegex((ValueError), 'reaction input'):
-            self.validations.ValidateReaction(message)
+            validations.ValidateReaction(message)
 
     def test_reaction_recursive(self):
         message = schema.Reaction()
         with self.assertRaisesRegex((ValueError), 'reaction input'):
-            self.validations.ValidateMessage(message, recurse=False)
+            validations.ValidateMessage(message)
         dummy_input = message.inputs['dummy_input']
+        self.assertEqual(validations.ValidateMessage(message, recurse=False), 
+            message)
         with self.assertRaisesRegex((ValueError), 'component'):
-            self.validations.ValidateMessage(message)
+            validations.ValidateMessage(message)
         dummy_component = dummy_input.components.add()
         with self.assertRaisesRegex((ValueError), 'identifier'):
-            self.validations.ValidateMessage(message)
+            validations.ValidateMessage(message)
         dummy_component.identifiers.add(type='CUSTOM')
         with self.assertRaisesRegex((ValueError), 'details'):
-            self.validations.ValidateMessage(message)
+            validations.ValidateMessage(message)
         dummy_component.identifiers[0].details = 'custom_identifier'
-        self.assertEqual(self.validations.ValidateMessage(message), message)
+        self.assertEqual(validations.ValidateMessage(message), message)
         outcome = message.outcomes.add()
         analysis = outcome.analyses['dummy_analysis']
-        self.assertEqual(self.validations.ValidateMessage(message), message)
+        self.assertEqual(validations.ValidateMessage(message), message)
 
     def test_datetimes(self):
         message = schema.ReactionProvenance()
         message.experiment_start.value = '11 am'
         message.record_created.value = '10 am'
         with self.assertRaisesRegex((ValueError), 'after'):
-            self.validations.ValidateMessage(message)
+            validations.ValidateMessage(message)
         message.record_created.value = '11:15 am'
-        self.assertEqual(self.validations.ValidateMessage(message), message)
+        self.assertEqual(validations.ValidateMessage(message), message)
 
 if __name__ == '__main__':
     absltest.main()
