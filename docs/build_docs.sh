e6c661897f6a810bd09c3e722da821aa1702be2e
Steven Kearnes <kearnes@google.com>
2020-04-15 11:35:29 -0700

    Basic sphinx documentation (#80)
    * Add inputs
    
    * Add requirements.txt
    
    * set master_doc
    
    * Update protos.md

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -5,5 +5,11 @@
 
 set -ex
 
+# Build HTML for protocol buffers.
 docker pull pseudomuto/protoc-gen-doc
-docker run --rm -v "$(pwd)":/out -v "$(pwd)/../proto":/protos pseudomuto/protoc-gen-doc --doc_opt=html,index.html
+docker run --rm -v "$(pwd)":/out -v "$(pwd)/../proto":/protos \
+  pseudomuto/protoc-gen-doc --doc_opt=markdown,protos.md:test.proto
+
+# Run sphinx.
+pip install -U sphinx recommonmark sphinx-markdown-tables
+make clean html
