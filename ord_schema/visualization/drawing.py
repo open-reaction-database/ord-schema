b801d8618fee5625abcd2e5a2831771f2e2190e3
Connor Coley <connor.coley@gmail.com>
2020-04-30 18:18:21 -0400

    Refactoring reaction message visualization code

---
+++ ord_schema/visualization/drawing.py
@@ -0,0 +1,80 @@
+"""This module contains two molecular drawing functions to render SVGs or PNGs
+given an RDKit molecule object: mol_to_svg and mol_to_png."""
+
+import io
+import base64
+import numpy as np
+from PIL import Image, ImageOps
+
+try:
+    from rdkit import Chem
+    from rdkit.Chem import Draw
+    from rdkit.Chem import rdDepictor
+    from rdkit import __version__ as RDKIT_VERSION
+    rdDepictor.SetPreferCoordGen(True)
+except ImportError:
+    Chem = None
+    RDKIT_VERSION = None
+
+# pylint: disable=unsubscriptable-object
+
+def trim_image_whitespace(img, padding=0):
+    """This function takes a PIL image, img, and crops it to the minimum
+    rectangle based on its whiteness/transparency. 5 pixel padding used
+    automatically."""
+
+    # Convert to array
+    as_array = np.array(img)  # N x N x (r,g,b,a)
+
+    # Set previously-transparent pixels to white
+    if as_array.shape[2] == 4:
+        as_array[as_array[:, :, 3] == 0] = [255, 255, 255, 0]
+
+    as_array = as_array[:, :, :3]
+
+    # Content defined as non-white and non-transparent pixel
+    has_content = np.sum(as_array, axis=2, dtype=np.uint32) != 255 * 3
+    xs_nonzero, ys_nonzero = np.nonzero(has_content)
+
+    # Crop down
+    margin = 5
+    x_range = (max([min(xs_nonzero) - margin, 0]),
+               min([max(xs_nonzero) + margin, as_array.shape[0]]))
+    y_range = (max([min(ys_nonzero) - margin, 0]),
+               min([max(ys_nonzero) + margin, as_array.shape[1]]))
+    as_array_cropped = as_array[
+        x_range[0]:x_range[1], y_range[0]:y_range[1], 0:3]
+
+    img = Image.fromarray(as_array_cropped, mode='RGB')
+    return ImageOps.expand(img, border=padding, fill=(255, 255, 255))
+
+
+def mol_to_svg(mol, max_size=300, bond_length=25):
+    """Returns a string with an <svg> tag containing a rendering of the input
+    molecule. Note that the SVG is not cropped."""
+    rdDepictor.Compute2DCoords(mol)
+    Chem.Kekulize(mol)
+    drawer = Draw.MolDraw2DSVG(max_size, max_size)
+    drawer.drawOptions().fixedBondLength = bond_length
+    drawer.DrawMolecule(mol)
+    drawer.FinishDrawing()
+    return drawer.GetDrawingText()
+
+
+def mol_to_png(mol, max_size=1000, bond_length=25, png_quality=70):
+    """Returns a string with an <img> tag containing a rendering of the input
+    molecule."""
+    drawer = Draw.MolDraw2DCairo(max_size, max_size)
+    drawer.drawOptions().fixedBondLength = bond_length
+    drawer.DrawMolecule(mol)
+    drawer.FinishDrawing()
+    temp = io.BytesIO()
+    temp.write(drawer.GetDrawingText())
+    temp.seek(0)
+    img = Image.open(temp)
+    img = trim_image_whitespace(img, padding=10)
+    output = io.BytesIO()
+    img.save(output, format='png', quality=png_quality)
+    output.seek(0)
+    b64 = base64.b64encode(output.getvalue())
+    return f'<img src="data:image/png;base64,{b64.decode("UTF-8")}"/>'
---
+++ ord_schema/visualization/generate_text.py
@@ -0,0 +1,97 @@
+"""Text generation script for Reaction messages.
+
+This script is meant to convert a Reaction message into a full-fledged text
+paragraph that would be appropriate for inclusion in a Supplemental Information
+document for a publication.
+
+Example usage:
+* For normal operation from a pbtxt
+  $ python generate_text.py --input_reaction=reaction.pb --input_format=pbtxt
+      --type text
+"""
+
+import os
+import re
+import jinja2
+
+from absl import app
+from absl import flags
+
+from ord_schema import message_helpers
+from ord_schema.proto import reaction_pb2
+from ord_schema.visualization import filters
+
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string('input_file', None,
+                    'File containing a Reaction message.')
+flags.DEFINE_enum('input_format', 'binary',
+                  [f.value for f in message_helpers.MessageFormats],
+                  'Input message format.')
+flags.DEFINE_enum('type', 'text', ['text', 'html'],
+                  'Text or HTML output format.')
+flags.DEFINE_string('output', None, 'Filename for output Dataset.')
+
+
+# pylint: disable=redefined-outer-name
+with open(os.path.join(os.path.dirname(__file__), 'template.html'), 'r') as fid:
+    _HTML_TEMPLATE = fid.read()
+
+with open(os.path.join(os.path.dirname(__file__), 'template.txt'), 'r') as fid:
+    _TEXT_TEMPLATE = fid.read()
+
+
+def _generate(reaction, template_string, line_breaks):
+    """Renders a Jinja2 template string with a reaction message.
+
+    Args:
+        reaction: a Reaction message.
+        template_string: Jinja template string.
+        line_breaks: Whether to keep line breaks.
+
+    Returns:
+        Rendered string.
+    """
+    env = jinja2.Environment(loader=jinja2.BaseLoader())
+    env.filters.update(filters.TEMPLATE_FILTERS)
+    template = env.from_string(template_string)
+    text = template.render(reaction=reaction)
+
+    # Fix line breaks, extra spaces, "a" versus "an"
+    if not line_breaks:
+        text = ''.join(text.strip().splitlines())
+    text = re.sub(r'[ ]{2,}', ' ', text)
+    text = re.sub(r' a ([aeiouAEIOU])', r' an \1', text)
+    text = re.sub(r'[ ]([\.\;\)\,]){1,2}', r'\1', text)
+    return text
+
+
+def generate_text(reaction):
+    """Generates a textual reaction description."""
+    return _generate(reaction, _TEXT_TEMPLATE, False)
+
+
+def generate_html(reaction):
+    return _generate(reaction, _HTML_TEMPLATE, True)
+
+
+def main(argv):
+    del argv  # Only used by app.run()
+    reaction = message_helpers.load_message(
+        FLAGS.input_file, reaction_pb2.Reaction, FLAGS.input_format)
+
+    if FLAGS.type == 'html':
+        text = generate_html(reaction)
+    elif FLAGS.type == 'text':
+        text = generate_text(reaction)
+
+    if FLAGS.output:
+        with open(FLAGS.output, 'w') as fid:
+            fid.write(text)
+    else:
+        print(text)
+
+
+if __name__ == '__main__':
+    flags.mark_flag_as_required('input_file')
+    app.run(main)
