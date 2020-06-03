83660ca31481507b9325eb650c75f7b025da9266
Anton Kast <akast@google.com>
2020-06-02 17:46:01 -0700

    Restore whitespace added by addlicense.

--- editor/serve.sh
+++ editor/serve.sh
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 export PYTHONPATH=py:gen/py
 export FLASK_APP=serve.py
 export FLASK_ENV=development
