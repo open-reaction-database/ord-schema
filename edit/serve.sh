d8b83fce5c6f40c9a3a25c97d1ddde132426d660
Anton Kast <akast@google.com>
2020-06-02 18:37:49 -0700

    Migrate the ORD editor from git-on-borg.
    Rename "edit" -> "editor".
    
    Clarify "./configure && make" for protobuf.
    
    Remove .DS_Store accident.

--- edit/serve.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=py:gen/py
-export FLASK_APP=serve.py
-export FLASK_ENV=development
-
-python -m flask run
-# Caution.
-# python -m flask run --host=0.0.0.0 --port=80
