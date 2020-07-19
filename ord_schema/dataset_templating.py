bc58b42bf46a2ce7bf97688f4a1dce273950f51c
Connor Coley <connor.coley@gmail.com>
2020-07-19 14:54:50 -0400

    First pass at templating dataset generation

---
+++ ord_schema/dataset_templating.py
@@ -0,0 +1,142 @@
+# Copyright 2020 Open Reaction Database Project Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Script and functions for enumerating a dataset using a template reaction and
+a spreadsheet file.
+
+The templating code has specific expectations for how the reaction pbtxt and
+spreadsheet are defined, namely that placeholder values in the pbtxt begin and
+end with a "$" (dollar sign) and that these match a unique column header in the
+spreadsheet file.
+
+Example usage:
+
+* For normal dataset generation from a template reaction:
+  $ python dataset_templating.py --input_template=my_reaction.pbtxt
+      --input_spreadsheet=my_experiments.csv --output=my_dataset.pbtxt
+"""
+
+import glob
+import os
+import subprocess
+import uuid
+import re
+import pandas as pd
+
+from absl import app
+from absl import flags
+from absl import logging
+
+from google.protobuf import text_format
+
+from ord_schema import validations
+from ord_schema import message_helpers
+from ord_schema.proto import dataset_pb2
+from ord_schema.proto import reaction_pb2
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string('input_template', None,
+                    'Path to a Reaction pbtxt file defining a template.')
+flags.DEFINE_string('input_spreadsheet', None,
+                    'Path to a spreadsheet file (with a header row) defining '
+                    'values to replace placeholders in the input_template.')
+flags.DEFINE_string('output', None, 'Filename for output Dataset.')
+flags.DEFINE_boolean('validate', True, 'If True, validate Reaction protos.')
+
+
+def _read_spreadsheet(file_name):
+    """Reads a spreadsheet file."""
+    _, suffix = os.path.splitext(file_name)
+    if suffix in ['xls', 'xlsx']:
+        return pd.read_excel(file_name, dtype=str)
+    return pd.read_csv(file_name, dtype=str)
+
+
+def _replace(string, substitutions):
+    pattern = re.compile('|'.join(map(re.escape, substitutions.keys())))
+    return pattern.sub(lambda match: substitutions[match.group(0)], string)
+
+
+def generate_datset(template_string, df, validate=True):
+    """Generates a Dataset from a template reaction formatted as text according
+    to entries in a pd.Dataframe.
+
+    Inputs:
+        template_string: The contents of a Reaction pbtxt where placeholder
+            values to be replaced are defined between dollar sign. For example,
+            a SMILES identifier value could be "$product_smiles$". Spaces
+            are not allowed.
+        df: Pandas Dataframe where each row corresponds to one reaction and
+            column names match placeholders in the template_string.
+        validate: Optional Boolean controlling whether Reaction messages should
+            be validated as they are defined. Defaults to True.
+
+    Returns:
+        A Dataset message.
+
+    Raises:
+        ValueError: If there is no match for a placeholder string in df.
+        ValueError: If validate is True and there are validation errors when
+            validating an enumerated Reaction message.
+
+    """
+    
+    placeholders = set(re.findall(r'\$\w+\$', template_string))
+    for placeholder in placeholders:
+        if placeholder not in df.columns:
+            # Allow "$my_placeholder$" to match "my_placeholder" in df.
+            if placeholder[1:-1] not in df.columns:
+                raise ValueError(f'Placeholder {placeholder} not found as a'
+                                 ' column in dataset spreadsheet')
+            df.rename(columns={placeholder[1:-1]: placeholder}, inplace=True)
+
+    reactions = []
+    for _,substitutions in df[placeholders].iterrows():
+        reaction_text = _replace(template_string, substitutions)
+        reaction = text_format.Parse(reaction_text, reaction_pb2.Reaction())
+        if validate:
+            errors = validations.validate_message(reaction,
+                                                  raise_on_error=False)
+            if errors:
+                raise ValueError(f'Enumerated Reaction is not valid: {errors}')
+        reactions.append(reaction)
+
+    return dataset_pb2.Dataset(reactions=reactions)
+
+
+def main(argv):
+    del argv  # Only used by app.run().
+
+    if not FLAGS.input_template:
+        raise ValueError('input_template must be defined')
+    with open(FLAGS.input_template, 'r') as fid:
+        template_string = fid.read()
+    if not FLAGS.input_spreadsheet:
+        raise ValueError('input_spreadsheet must be defined')
+    df = _read_spreadsheet(FLAGS.input_spreadsheet)
+
+    logging.info('generating new Dataset from %s and %s', FLAGS.input_template,
+                 FLAGS.input_spreadsheet)
+    dataset = generate_dataset(template_string, df, validate=FLAGS.validate)
+
+    if FLAGS.output:
+        output_filename = FLAGS.output
+    else:
+        basename, suffix = os.path.splitext(FLAGS.input_spreadsheet)
+        output_filename = os.path.join(f'{basename}.pbtxt')
+    logging.info('writing new Dataset to %s', output_filename)
+    message_helpers.write_message(dataset, output_filename)
+
+
+if __name__ == '__main__':
+    app.run(main)
---
+++ ord_schema/dataset_templating_test.py
@@ -0,0 +1,105 @@
+# Copyright 2020 Open Reaction Database Project Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for ord_schema.message_helpers."""
+
+import os
+import tempfile
+import pandas as pd
+
+from absl import flags
+from absl.testing import absltest
+from absl.testing import parameterized
+from google.protobuf import text_format
+
+from ord_schema import message_helpers
+from ord_schema import dataset_templating
+from ord_schema.proto import reaction_pb2
+from ord_schema.proto import dataset_pb2
+
+
+class DatasetTemplatingTest(absltest.TestCase):
+    def setUp(self):
+        super().setUp()
+        
+        message = reaction_pb2.Reaction()
+        dummy_input = message.inputs['my_input']
+        outcome = message.outcomes.add()
+        component = dummy_input.components.add()
+        component.identifiers.add(type='SMILES', value='CCO')
+        component.is_limiting = True
+        component.mass.value = 1
+        component.mass.units = reaction_pb2.Mass.GRAM
+        outcome.conversion.value = 75
+        self.valid_reaction = message
+
+
+    def test_valid_templating(self):
+        template_string = text_format.MessageToString(self.valid_reaction)
+        template_string = template_string.replace('value: "CCO"', 
+                                                  'value: "$SMILES$"')
+        template_string = template_string.replace('value: 75',
+                                                  'value: $conversion$')
+        df = pd.DataFrame.from_dict({
+            '$SMILES$': ['CCO', 'CCCO', 'CCCCO'],
+            '$conversion$': ['75', '50', '30'],
+        })
+        dataset = dataset_templating.generate_datset(template_string, df)
+        exptected_reactions = []
+        for smiles, conversion in zip(['CCO', 'CCCO', 'CCCCO'], [75, 50, 30]):
+            reaction = reaction_pb2.Reaction()
+            reaction.CopyFrom(self.valid_reaction)
+            reaction.inputs['my_input'].components[0].identifiers[0].value = smiles
+            reaction.outcomes[0].conversion.value = conversion
+            exptected_reactions.append(reaction)
+        expected_dataset = dataset_pb2.Dataset(reactions=exptected_reactions)
+        self.assertEqual(dataset, expected_dataset)
+
+    def test_invalid_templating(self):
+        template_string = text_format.MessageToString(self.valid_reaction)
+        template_string = template_string.replace('value: "CCO"', 
+                                                  'value: "$SMILES$"')
+        template_string = template_string.replace('value: 75',
+                                                  'value: $conversion$')
+        df = pd.DataFrame.from_dict({
+            '$SMILES$': ['CCO', 'CCCO', 'CCCCO'],
+            '$conversion$': ['75', '50', '-5'],
+        })
+        exptected_reactions = []
+        for smiles, conversion in zip(['CCO', 'CCCO', 'CCCCO'], [75, 50, -5]):
+            reaction = reaction_pb2.Reaction()
+            reaction.CopyFrom(self.valid_reaction)
+            reaction.inputs['my_input'].components[0].identifiers[0].value = smiles
+            reaction.outcomes[0].conversion.value = conversion
+            exptected_reactions.append(reaction)
+        expected_dataset = dataset_pb2.Dataset(reactions=exptected_reactions)
+        with self.assertRaisesRegex(ValueError, 'is not valid'):
+            dataset_templating.generate_datset(template_string, df)
+        dataset = dataset_templating.generate_datset(template_string, df,
+                                                     validate=False)
+        self.assertEqual(dataset, expected_dataset)
+
+    def test_bad_placeholders(self):
+        template_string = text_format.MessageToString(self.valid_reaction)
+        template_string = template_string.replace('value: "CCO"', 
+                                                  'value: "$SMILES$"')
+        template_string = template_string.replace('value: 75',
+                                                  'value: $conversion$')
+        df = pd.DataFrame.from_dict({
+            '$SMILES$': ['CCO', 'CCCO', 'CCCCO'],
+        })
+        with self.assertRaisesRegex(ValueError, '\$conversion\$ not found'):
+            dataset_templating.generate_datset(template_string, df)
+
+if __name__ == '__main__':
+    absltest.main()
