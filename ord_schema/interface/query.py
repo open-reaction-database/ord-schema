32221c43b4be258d2f8ea45654e39cf722238ef7
Steven Kearnes <kearnes@google.com>
2020-07-21 14:04:57 -0600

    Add support for similarity searching (#238)
    * Cleanup OrdPostgres usage
    
    * Add support for similarity searching
    
    * Update docstring

--- ord_schema/interface/query.py
+++ ord_schema/interface/query.py
@@ -39,24 +39,12 @@ class OrdPostgres:
             host: Text host name.
             port: Integer port.
         """
-        self._dbname = dbname
-        self._user = user
-        self._password = password
-        self._host = host
-        self._port = port
-        self._connection = None
-
-    def __enter__(self):
-        self._connection = psycopg2.connect(dbname=self._dbname,
-                                            user=self._user,
-                                            password=self._password,
-                                            host=self._host,
-                                            port=self._port)
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        del exc_type, exc_val, exc_tb  # Unused.
-        self._connection.close()
+        self._connection = psycopg2.connect(dbname=dbname,
+                                            user=user,
+                                            password=password,
+                                            host=host,
+                                            port=port)
+        self._connection.set_session(readonly=True)
 
     def cursor(self):
         return self._connection.cursor()
@@ -92,9 +80,8 @@ class OrdPostgres:
             {f'LIMIT {limit}' if limit else ''};"""
         logging.info(query)
         with self._connection, self.cursor() as cursor:
-            # NOTE(kearnes): I don't like that we are committing this change to
-            # the database after the context manager exits. It's possible to
-            # auto-rollback by raising an exception in the context manager.
+            # NOTE(kearnes): We call rollback() to reset this change before
+            # exiting the context manager (which triggers a commit).
             cursor.execute(f'SET rdkit.do_chiral_sss={do_chiral_sss};')
             cursor.execute(query)
             reactions = []
@@ -103,4 +90,44 @@ class OrdPostgres:
                 reaction = reaction_pb2.Reaction.FromString(
                     binascii.unhexlify(serialized.tobytes()))
                 reactions.append(reaction)
+            self._connection.rollback()
+        return dataset_pb2.Dataset(reactions=reactions)
+
+    def similarity_search(self, smiles, table, limit=100, threshold=0.5):
+        """Performs a Tanimoto similarity search.
+
+        Args:
+            smiles: Text SMILES.
+            table: Text SQL table name.
+            limit: Integer maximum number of matches to return.
+            threshold: Float similarity threshold.
+
+        Returns:
+            Dataset proto containing matched Reactions.
+        """
+        if 'reactions' in table:
+            column = 'rdfp'
+            function = 'reaction_difference_fp'
+        else:
+            column = 'mfp2'
+            function = 'morganbv_fp'
+        query = f"""
+            SELECT DISTINCT A.reaction_id, A.serialized 
+            FROM reactions A 
+            INNER JOIN {table} B ON A.reaction_id = B.reaction_id
+            WHERE B.{column}%{function}('{smiles}')
+            {f'LIMIT {limit}' if limit else ''};"""
+        logging.info(query)
+        with self._connection, self.cursor() as cursor:
+            # NOTE(kearnes): We call rollback() to reset this change before
+            # exiting the context manager (which triggers a commit).
+            cursor.execute(f'SET rdkit.tanimoto_threshold={threshold};')
+            cursor.execute(query)
+            reactions = []
+            for result in cursor:
+                _, serialized = result
+                reaction = reaction_pb2.Reaction.FromString(
+                    binascii.unhexlify(serialized.tobytes()))
+                reactions.append(reaction)
+            self._connection.rollback()
         return dataset_pb2.Dataset(reactions=reactions)
