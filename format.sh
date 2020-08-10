ca5877cba43e61863bb6c2a0e9f11860e9b8df4a
Steven Kearnes <kearnes@google.com>
2020-08-10 11:58:44 -0600

    Move linters and formatters to cleanup.yml

---
+++ format.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright 2020 Open Reaction Database Project Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Runs code formatters.
+set -ex
+# See https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/.
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+# Add missing license headers.
+# go get github.com/google/addlicense
+addlicense -c "Open Reaction Database Project Authors" -l apache "${ROOT_DIR}"
+# Format python.
+# pip install yapf
+yapf -p -r "${ROOT_DIR}" --exclude="*_pb2.py" --in-place
+# Format javascript and proto.
+# apt install clang-format
+# NOTE(kearnes): Make sure you have version 10 or higher!
+find "${ROOT_DIR}" -name '*.js' -exec clang-format -i --style=file {} +
+find "${ROOT_DIR}" -name '*.proto' -exec clang-format -i --style=file {} +
