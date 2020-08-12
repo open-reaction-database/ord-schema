84ac6b42170d76b3b2d869cdfb10c34f3bde63c5
Steven Kearnes <kearnes@google.com>
2020-08-12 10:39:14 -0600

    Make format.sh friendlier

--- format.sh
+++ format.sh
@@ -18,13 +18,28 @@ set -ex
 # See https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/.
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 # Add missing license headers.
-# go get github.com/google/addlicense
-addlicense -c "Open Reaction Database Project Authors" -l apache "${ROOT_DIR}"
+if command -v go &> /dev/null; then
+  go run github.com/google/addlicense \
+    -c "Open Reaction Database Project Authors" \
+    -l apache "${ROOT_DIR}"
+else
+  echo "Please install Go; see https://golang.org/doc/install"
+fi
 # Format python.
-# pip install yapf
+if ! command -v yapf &> /dev/null; then
+  pip install yapf
+fi
 yapf -p -r "${ROOT_DIR}" --exclude="*_pb2.py" --in-place
 # Format javascript and proto.
-# apt install clang-format
-# NOTE(kearnes): Make sure you have version 10 or higher!
-find "${ROOT_DIR}" -name '*.js' -exec clang-format -i --style=file {} +
-find "${ROOT_DIR}" -name '*.proto' -exec clang-format -i --style=file {} +
+if command -v clang-format-10 &> /dev/null; then
+  find "${ROOT_DIR}" -name '*.js' -exec clang-format-10 -i --style=file {} +
+  find "${ROOT_DIR}" -name '*.proto' -exec clang-format-10 -i --style=file {} +
+elif command -v clang-format &> /dev/null; then
+  # NOTE(kearnes): Make sure you have version 10 or higher!
+  find "${ROOT_DIR}" -name '*.js' -exec clang-format -i --style=file {} +
+  find "${ROOT_DIR}" -name '*.proto' -exec clang-format -i --style=file {} +
+else
+  echo "Please install clang-format:"
+  echo "  Linux: apt install clang-format-10"
+  echo "  MacOS: brew install clang-format"
+fi
