3527103fce60eb369515297751dd6764121eab68
Anton Kast <akast@google.com>
2020-08-24 16:38:20 -0700

    Add a search interface to the Postgres DB. (#286)
    * Add a search interface to the Postgres DB.
    
    First start Postgres, e.g. with docker/serve.sh. Then start
    the web server with ./serve.sh. A demo is running currently at
    http://104.154.95.96:8080/.
    
    The user interface design is not final; it is just meant to demonstrate the
    query semantics.
    
    * lint

---
+++ ord_schema/interface/search.py
@@ -0,0 +1,126 @@
+# Copyright 2020 Open Reaction Database Project Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Query web interface to the Open Reaction Database in Postgres.
+
+The client is stateless. Full information is in the URL so results can be
+linked.
+
+There are three kinds of query.
+
+  inputs and output SMILES or SMARTS
+
+    These match against individual reagents and results. Each SMILES expression
+    can be freely specified for "exact", "substructure", or "similarity"
+    matching. SMARTS expressions do not allow such a specification.
+
+  reaction ID
+
+    Matches either zero or one reactions. The ID has the form,
+      "ord-<32-bit hex>".
+
+  reaction SMILES
+
+    Matches against RDKit fingerprints.
+
+Query parameters are communicated in URL GET params.
+
+  input=<pattern;(exact|substructure|similarity|smarts)>
+
+  output=<pattern;(exact|substructure|similarity|smarts)>
+
+    The token after the semicolon specifies the matching criterion. The default
+    is "exact". The pattern is a SMILES string, unless the token is "smarts" in
+    which case the pattern is a SMARTS string.
+
+    The "input" param may be repeated. The "output" param may not.
+
+  reaction_id=<id>
+
+  reaction_smiles=<smiles>
+
+If multiple conditions are given, then the query is interpreted as conjunction.
+
+All query parameters are assumed to be URL-encoded.
+"""
+
+import flask
+
+import query
+
+app = flask.Flask(__name__, template_folder='.')
+
+
+@app.route('/')
+def show_root():
+    """Shows the web form.
+
+    If there are query params, then the query is executed and the form is
+    populated with the results. The form fields are populated with the params.
+    """
+    reaction_ids = flask.request.args.get('reaction_ids')
+    reaction_smiles = flask.request.args.get('reaction_smiles')
+    inputs = flask.request.args.getlist("input")
+    output = flask.request.args.get('output')
+    similarity = flask.request.args.get('similarity')
+
+    predicate = query.Predicate()
+
+    if reaction_ids is not None:
+        for reaction_id in reaction_ids.split(','):
+            predicate.add_reaction_id(reaction_id)
+
+    if reaction_smiles is not None:
+        predicate.set_reaction_smiles(reaction_smiles)
+
+    in_splits = [i.split(';', 1) for i in inputs] if inputs else []
+    for smiles, mode_name in in_splits:
+        mode = query.Predicate.MatchMode.from_name(mode_name)
+        predicate.add_input(smiles, mode)
+
+    if output is not None:
+        smiles, mode_name = output.split(';', 1)
+        mode = query.Predicate.MatchMode.from_name(mode_name)
+        predicate.set_output(smiles, mode)
+
+    if similarity is not None:
+        predicate.set_tanimoto_threshold(float(similarity))
+
+    if reaction_ids or reaction_smiles or inputs or output:
+        db = connect()
+        reaction_ids = db.predicate_search_ids(predicate)
+    else:
+        reaction_ids = []
+    return flask.render_template('search.html',
+                                 reaction_ids=reaction_ids,
+                                 predicate=predicate.json())
+
+
+@app.route('/id/<reaction_id>')
+def show_id(reaction_id):
+    """Returns the pbtxt of a single reaction as plain text."""
+    predicate = query.Predicate()
+    predicate.add_reaction_id(reaction_id)
+    db = connect()
+    dataset = db.predicate_search(predicate)
+    if len(dataset.reactions) == 0:
+        return flask.abort(404)
+    return flask.Response(str(dataset.reactions[0]), mimetype='text/plain')
+
+
+def connect():
+    return query.OrdPostgres(dbname='ord',
+                             user='ord-postgres',
+                             password='ord-postgres',
+                             host='localhost',
+                             port=5432)
---
+++ ord_schema/interface/search.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2020 Open Reaction Database Project Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Launch a web server that connects to the local Postgres and conducts queries
+# on the ORD. By default the web server is at http://<host>:5000/
+#
+# First launch local Postgres like this:
+#
+#   $ ./docker/serve.sh
+
+export PYTHONPATH=.:../build/lib
+export FLASK_APP=search.py
+export FLASK_ENV=development
+
+python -m flask run $@
