7ae5b025c675e1062653d9d2788969700eaad32b
Steven Kearnes <kearnes@google.com>
2020-10-10 17:14:09 -0600

    Fix test

--- ord_schema/process_dataset_test.py
+++ ord_schema/process_dataset_test.py
@@ -94,10 +94,11 @@ class ProcessDatasetTest(absltest.TestCase):
         with flagsaver.flagsaver(input_pattern=self.dataset1_filename,
                                  update=True,
                                  output=output,
+                                 write_binary=False,
                                  base='main'):
             process_dataset.main(())
         self.assertTrue(os.path.exists(output))
-        self.assertTrue(
+        self.assertFalse(
             os.path.exists(os.path.join(self.test_subdirectory, 'output.pb')))
         dataset = message_helpers.load_message(output, dataset_pb2.Dataset)
         self.assertLen(dataset.reactions, 1)
@@ -222,6 +223,9 @@ class SubmissionWorkflowTest(absltest.TestCase):
         self.assertNotEmpty(dataset.dataset_id)
         self.assertLen(dataset.reactions, 1)
         self.assertNotEmpty(dataset.reactions[0].reaction_id)
+        # Check for binary output.
+        root, ext = os.path.splitext(filenames[0])
+        self.assertTrue(os.path.exists(root + '.pb'))
 
     def test_add_sharded_dataset(self):
         reaction = reaction_pb2.Reaction()
