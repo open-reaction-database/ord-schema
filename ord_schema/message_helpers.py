30167fb8c0ae8ea61e83c9c78f6b8515b78c8916
Michael Maser <mmaser@caltech.edu>
2020-11-25 11:21:41 -0800

    yapf

--- ord_schema/message_helpers.py
+++ ord_schema/message_helpers.py
@@ -586,7 +586,7 @@ def set_dative_bonds(mol, from_atoms=('N', 'P')):
                         f'with SMILES {Chem.MolToSmiles(mol)}. If this is '
                         f'a datively bound metal-carbene complex, '
                         f'the positive charge should be removed from '
-                        f'the SMILES string before converting dative bonds', 
+                        f'the SMILES string before converting dative bonds',
                         validations.ValidationWarning)
                 if (nbr.GetTotalValence() + nbr.GetFormalCharge() !=
                         p_table.GetDefaultValence(nbr_atom) and
