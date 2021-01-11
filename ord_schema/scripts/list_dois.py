e464e2b52c46a404520cd05cefd67cf21b49e395
Steven Kearnes <kearnes@google.com>
2021-01-11 11:21:26 -0700

    Fetch references for DOIs

--- ord_schema/scripts/list_dois.py
+++ ord_schema/scripts/list_dois.py
@@ -27,6 +27,7 @@ import urllib.parse
 from absl import app
 from absl import flags
 from absl import logging
+import requests
 
 from ord_schema import message_helpers
 from ord_schema.proto import dataset_pb2
@@ -50,13 +51,19 @@ def main(argv):
         doi_set = set()
         for reaction in dataset.reactions:
             # Some poorly-validated DOI entries start with 'doi:'...
-            match = re.fullmatch(r'(?:(?:doi)|(?:DOI))?:?\s*(.*)',
+            match = re.fullmatch(r'(?:(?:doi)|(?:DOI))?:?\s*(.+)',
                                  reaction.provenance.doi)
+            if not match:
+                continue  # No DOI.
             doi_set.add(match.group(1))
         for doi in doi_set:
             dois[doi].append(dataset_id)
     for doi in sorted(dois):
-        print(f'* [{doi}](https://doi.org/{doi})')
+        url = f'https://doi.org/{doi}'
+        reference = requests.get(
+            url, headers={'Accept': 'text/x-bibliography; style=nature'})
+        citation = f'{reference.content.decode().strip()[2:]} [doi: {doi}]({url})'
+        print(f'* {citation}')
         for dataset in sorted(dois[doi]):
             url = urllib.parse.urljoin(
                 _PREFIX,
