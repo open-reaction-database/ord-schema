6e9a88cd0710f0021f9084129c3928503b2c1591
Steven Kearnes <kearnes@google.com>
2021-03-19 16:06:34 -0600

    updates

--- setup.py
+++ setup.py
@@ -44,29 +44,28 @@ class BuildPyCommand(build_py.build_py):
         build_py.build_py.run(self)
 
 
-if __name__ == '__main__':
-    setuptools.setup(
-        name='ord-schema',
-        description='Schema for the Open Reaction Database',
-        url='https://github.com/Open-Reaction-Database/ord-schema',
-        license='Apache License, Version 2.0',
-        packages=setuptools.find_packages(),
-        package_data={
-            'ord_schema.visualization': ['template.html', 'template.txt'],
-        },
-        install_requires=[
-            'absl-py>=0.9.0',
-            'flask>=1.1.2',
-            'numpy>=1.18.1',
-            'openpyxl>=3.0.5',
-            'pandas>=1.0.4',
-            'protobuf>=3.13.0',
-            'protoc-wheel-0>=3.14.0',
-            'pygithub>=1.51',
-            'python-dateutil>=1.10.0',
-            'jinja2>=2.0.0',
-            'xlrd<2.0.0',
-            'xlwt>=1.3.0',
-            'joblib>=1.0.0',
-        ],
-        cmdclass={'build_py': BuildPyCommand})
+setuptools.setup(
+    name='ord-schema',
+    description='Schema for the Open Reaction Database',
+    url='https://github.com/Open-Reaction-Database/ord-schema',
+    license='Apache License, Version 2.0',
+    packages=setuptools.find_packages(),
+    package_data={
+        'ord_schema.visualization': ['template.html', 'template.txt'],
+    },
+    install_requires=[
+        'absl-py>=0.9.0',
+        'flask>=1.1.2',
+        'numpy>=1.18.1',
+        'openpyxl>=3.0.5',
+        'pandas>=1.0.4',
+        'protobuf>=3.13.0',
+        'protoc-wheel-0>=3.14.0',
+        'pygithub>=1.51',
+        'python-dateutil>=1.10.0',
+        'jinja2>=2.0.0',
+        'xlrd<2.0.0',
+        'xlwt>=1.3.0',
+        'joblib>=1.0.0',
+    ],
+    cmdclass={'build_py': BuildPyCommand})
