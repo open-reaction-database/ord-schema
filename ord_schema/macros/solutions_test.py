e600737447886b2cc73c77a64610ec6aaddcc448
Brian Lee <brianklee@google.com>
2021-08-18 17:42:45 -0400

    Formatting fixes

--- ord_schema/macros/solutions_test.py
+++ ord_schema/macros/solutions_test.py
@@ -21,17 +21,21 @@ from ord_schema.proto import reaction_pb2
 from ord_schema.macros import solutions
 from ord_schema import validations
 
+
 class SolutionsTest(parameterized.TestCase, absltest.TestCase):
 
     @parameterized.parameters(
-        itertools.product(['[Na+].[Cl-]', None], ['1M', None], ['1L', None])
-    )
+        itertools.product(['[Na+].[Cl-]', None], ['1M', None], ['1L', None]))
     def test_simple_solution(self, solute_smiles, concentration, volume):
         """Test that the macro never generates illegal solution configurations."""
-        solution_components = solutions.simple_solution(solvent_smiles='O', solute_smiles=solute_smiles,
-        concentration=concentration, volume=volume)
+        solution_components = solutions.simple_solution(
+            solvent_smiles='O',
+            solute_smiles=solute_smiles,
+            concentration=concentration,
+            volume=volume)
         for component in solution_components:
             validations.validate_message(component)
+
 
 if __name__ == '__main__':
     absltest.main()
