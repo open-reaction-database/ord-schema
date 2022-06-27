13c6d1f7421db8023557e487c04bd21678512b32
Steven Kearnes <skearnes@gmail.com>
2022-06-26 22:03:34 -0400

    install package during build

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-from recommonmark import transform
 
 # -- Project information -----------------------------------------------------
 
