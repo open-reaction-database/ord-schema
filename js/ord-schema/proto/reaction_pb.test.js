5607db3e00cfa109f9da83201240ae82226d5674
Steven Kearnes <skearnes@relaytx.com>
2022-12-30 13:36:43 -0500

    add npm package

---
+++ js/ord-schema/proto/reaction_pb.test.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2022 Open Reaction Database Project Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const ord_schema = require('..');
+
+test('simple', () => {
+    const reaction = new ord_schema.reaction_pb.Reaction();
+    const identifier = reaction.addIdentifiers();
+    identifier.setValue('C(C)Cl.Br>>C(C)Br.Cl');
+    identifier.setType(ord_schema.reaction_pb.ReactionIdentifier.ReactionIdentifierType.REACTION_SMILES);
+    expect(reaction.getIdentifiersList()).toHaveLength(1);
+    expect(reaction.getSetup()).toBeUndefined();
+});
