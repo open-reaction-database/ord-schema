aef8ad4ae4d7ece2726833a427963fe2b663636f
Steven Kearnes <skearnes@relaytx.com>
2023-01-27 12:09:06 -0500

    Add JavaScript wrappers (#665)
    * Add JavaScript wrappers
    
    * reorganize proto definitions
    
    * add npm package
    
    * publish to npm
    
    * add node test
    
    * delete package-lock.json
    
    * more specific search/replace
    
    * gitignore
    
    * updates
    
    * single namespace
    
    * update headers
    
    * Revert "update headers"
    
    This reverts commit e0cc4d5697ce223ceb096f11d7916deb38c587b6.
    
    * fix ignore
    
    * add tests for publish
    
    * headers
    
    * move to tests
    
    * formatting
    
    * pytype
    
    * pytype
    
    * pytype

---
+++ js/ord-schema/proto/dataset_pb.test.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2022 Open Reaction Database Project Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const ord = require('..');
+
+test('round-trip', () => {
+    const dataset = new ord.Dataset();
+    dataset.setName('test');
+    dataset.setDescription('test dataset');
+    // Add a reaction directly to the dataset.
+    const reaction1 = dataset.addReactions();
+    const identifier1 = reaction1.addIdentifiers();
+    identifier1.setValue('C(C)Cl.Br>>C(C)Br.Cl');
+    identifier1.setType(ord.ReactionIdentifier.ReactionIdentifierType.REACTION_SMILES);
+    // Copy a reaction created elsewhere.
+    const reaction2 = new ord.Reaction();
+    const identifier2 = reaction2.addIdentifiers();
+    identifier2.setValue('amide coupling');
+    identifier2.setType(ord.ReactionIdentifier.ReactionIdentifierType.NAME);
+    dataset.addReactions(reaction2);
+    const serialized = dataset.serializeBinary();
+    const other = ord.Dataset.deserializeBinary(serialized);
+    expect(other.getName()).toBe('test');
+    expect(other.getDescription()).toBe('test dataset');
+    expect(other.getReactionsList()).toHaveLength(2);
+    expect(other.getReactionsList()[0].getIdentifiersList()[0].getType()).toBe(ord.ReactionIdentifier.ReactionIdentifierType.REACTION_SMILES);
+    expect(other.getReactionsList()[1].getIdentifiersList()[0].getType()).toBe(ord.ReactionIdentifier.ReactionIdentifierType.NAME);
+});
