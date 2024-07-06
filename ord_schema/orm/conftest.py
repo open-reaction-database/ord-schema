c349a39e74ea0f4a30c2de054d2dd60f9801f979
Steven Kearnes <skearnes@gmail.com>
2024-07-06 14:25:22 -0400

    revert profiling

--- ord_schema/orm/conftest.py
+++ ord_schema/orm/conftest.py
@@ -14,28 +14,40 @@
 
 """Pytest fixtures."""
 import os
+import re
 from typing import Iterator
 
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session
+from testing.postgresql import Postgresql
 
 from ord_schema.message_helpers import load_message
-from ord_schema.orm.database import add_dataset
-from ord_schema.orm.testing import get_test_engine
+from ord_schema.orm.database import add_dataset, prepare_database
 from ord_schema.proto import dataset_pb2
 
 
+@pytest.fixture(name="test_engine")
+def test_engine_fixture() -> Iterator[Engine]:
+    with Postgresql() as postgres:
+        # See https://docs.sqlalchemy.org/en/20/dialects/postgresql.html#module-sqlalchemy.dialects.postgresql.psycopg.
+        url = re.sub("postgresql://", "postgresql+psycopg://", postgres.url())
+        engine = create_engine(url, future=True)
+        yield engine
+
+
 @pytest.fixture(name="test_session")
-def test_session_fixture() -> Iterator[Session]:
+def test_session_fixture(test_engine) -> Iterator[Session]:
     datasets = [
         load_message(
             os.path.join(os.path.dirname(__file__), "testdata", "ord-nielsen-example.pbtxt"), dataset_pb2.Dataset
         )
     ]
-    with get_test_engine() as (engine, rdkit_cartridge):
-        with Session(engine) as session:
-            for dataset in datasets:
-                with session.begin():
-                    add_dataset(dataset, session, rdkit_cartridge=rdkit_cartridge)
-        with Session(engine) as session:
-            yield session
+    rdkit_cartridge = prepare_database(test_engine)
+    with Session(test_engine) as session:
+        for dataset in datasets:
+            with session.begin():
+                add_dataset(dataset, session, rdkit_cartridge=rdkit_cartridge)
+    with Session(test_engine) as session:
+        yield session
