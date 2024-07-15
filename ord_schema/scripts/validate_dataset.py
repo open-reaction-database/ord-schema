7a8774fcb4ce4e7b5b5aeddd05c874573abb7869
Steven Kearnes <skearnes@relaytx.com>
2024-07-15 13:28:06 -0400

    Parallel validation

--- ord_schema/scripts/validate_dataset.py
+++ ord_schema/scripts/validate_dataset.py
@@ -14,18 +14,21 @@
 """Validates a set of Dataset protocol buffers.
 
 Usage:
-    validate.py --input=<str> [--filter=<str>]
+    validate.py --input=<str> [--filter=<str> --n_jobs=<int>]
 
 Options:
     --input=<str>       Input pattern for Dataset protos
     --filter=<str>      Regex filename filter
+    --n_jobs=<int>      Number of parallel workers [default: 1]
 """
 import glob
 import re
 from collections.abc import Iterable
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 import docopt
 from rdkit import RDLogger
+from tqdm import tqdm
 
 from ord_schema import message_helpers, validations
 from ord_schema.logging import get_logger
@@ -43,18 +46,33 @@ def filter_filenames(filenames: Iterable[str], pattern: str) -> list[str]:
     return filtered_filenames
 
 
+def run(filename: str) -> None:
+    """Validates a single dataset."""
+    RDLogger.DisableLog("rdApp.*")  # Disable RDKit logging.
+    dataset = message_helpers.load_message(filename, dataset_pb2.Dataset)
+    validations.validate_datasets({filename: dataset})
+
+
 def main(kwargs):
     filenames = sorted(glob.glob(kwargs["--input"], recursive=True))
     logger.info("Found %d datasets", len(filenames))
     if kwargs["--filter"]:
         filenames = filter_filenames(filenames, kwargs["--filter"])
         logger.info("Filtered to %d datasets", len(filenames))
-    for filename in filenames:
-        logger.info("Validating %s", filename)
-        dataset = message_helpers.load_message(filename, dataset_pb2.Dataset)
-        validations.validate_datasets({filename: dataset})
+    futures = {}
+    failures = []
+    with ProcessPoolExecutor(int(kwargs["--n_jobs"])) as executor:
+        for filename in filenames:
+            future = executor.submit(run, filename=filename)
+            futures[future] = filename
+        for future in tqdm(as_completed(futures), total=len(futures)):
+            try:
+                future.result()
+            except validations.ValidationError as error:
+                failures.append((futures[future], error))
+    if failures:
+        raise ValueError(f"Dataset(s) failed validation: {failures}")
 
 
 if __name__ == "__main__":
-    RDLogger.DisableLog("rdApp.*")  # Disable RDKit logging.
     main(docopt.docopt(__doc__))
